e077b0000cb5418a558625c2ffd646baf6b0d56c
Upadhyay <810489@cognizant.com>
2019-10-24 14:25:26 +0530

    updated

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes=HEROES;
+  heroes:Hero[];
 
   selectedHero:Hero;
   onSelect(hero:Hero):void{
       this.selectedHero=hero;
   }
 
-  constructor() { }
+  constructor(private heroService:HeroService) { }
 
   ngOnInit() {
-    
+      this.getHeroes();
+  }
+
+  // getHeroes() : void {
+  //   this.heroes=this.heroService.getHeroes();
+  // }
+
+  getHeroes() : void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes=heroes);
   }
 
 }
